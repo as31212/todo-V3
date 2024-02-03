5e507f2c4664d1ae05bbdf1f9fc0255d397307a4
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-02-03 09:04:19 -0600

    you must add the edit feature

--- todo.js
+++ todo.js
@@ -3,7 +3,8 @@ const modalPageElement = document.getElementById("modal-page");
 const createEntryPageElement = document.getElementById("create-entry-page");
 const addEntryButtonElement = document.getElementById("add-entry-button");
 const entryContainerElement = document.getElementById("entry-container");
-const entriesObject = [];
+let entriesObject = [];
+
 
 //create entry button functionality, unsure if i need a counter as of right now, but I will remove it later if not needed
 const createEntry = () => {
@@ -17,7 +18,7 @@ const createEntry = () => {
     <input required id="date-input" type="date">
     <label for="description-input"><strong>Description</strong></label>
     <textarea cols="20" rows="10" required id="description-input"></textarea>
-    <button onclick="addTask()" id="add-task-button" class="large-button">Add Task</button>    
+    <button onclick="addTaskToObject(); updateEntryContainer()" id="add-task-button" class="large-button">Add Task</button>    
     `;
   createEntryPageElement.innerHTML = HTML;
 };
@@ -28,40 +29,85 @@ addEntryButtonElement.addEventListener("click", () => {
 
 //add task functionality
 
-const addTask = () => {
+const addTaskToObject = () => {
   const titleEntry = document.getElementById("title-input");
   const dateEntry = document.getElementById("date-input");
   const descriptionEntry = document.getElementById("description-input");
   entriesObject.push({
     title: `${titleEntry.value}`,
     dateEntry: `${dateEntry.value}`,
-    description: `${descriptionEntry.value}`,
+    description: `${descriptionEntry.value}`,    
   });
 
+  //updating the local storage 
+  localStorage.setItem('entryData',JSON.stringify(entriesObject));
+};
+
+//update entry container html
+const updateEntryContainer = ()=>{
+const counter = entriesObject.length-1;
+const title = entriesObject[counter].title;
+const date = entriesObject[counter].dateEntry;
+const description = entriesObject[counter].description;
+
   //resetting the page
+  if(entryContainerElement.classList.contains('hidden')){
   addEntryButtonElement.classList.toggle("hidden");
   entryContainerElement.classList.toggle("hidden");
   createEntryPageElement.innerHTML = "";
-
-  const counter = document.querySelectorAll(".entry");
+  }
+ 
 
   //creating HTML
   const HTML = `
   <div class="entry" id="entry-${counter}">
-  <label for="title-entry-${counter}"><strong>Title: </strong><p id="title-entry-${counter}">${titleEntry.value}</p></label>
+  <label for="title-entry-${counter}"><strong>Title: </strong><p id="title-entry-${counter}">${title}</p></label>
   <br>
-  <label for="date-entry-${counter}"><strong>Date: </strong><p id="date-entry-${counter}">${dateEntry.value}</p></label>
+  <label for="date-entry-${counter}"><strong>Date: </strong><p id="date-entry-${counter}">${date}</p></label>
   <br>
-  <label for="description-entry-${counter}"><strong>Description: </strong><p id="description-entry-${counter}">${descriptionEntry.value}</p></label>
+  <label for="description-entry-${counter}"><strong>Description: </strong><p id="description-entry-${counter}">${description}</p></label>
   <br>
   <button id="edit-button-${counter}">Edit</button>
-  <button id="delete-button-${counter}">Delete</button>
+  <button onclick="deleteEntry(${counter})" id="delete-button-${counter}">Delete</button>
   </div>
   `;
 
   entryContainerElement.insertAdjacentHTML("beforeend", HTML);
-  localStorage.setItem(JSON.stringify(entriesObject));
-};
+}
+
+//update base on local storage function
+
+const updateEntryContainerLocalStorage = (element)=>{
+  const title = entriesObject[element].title;
+  const date = entriesObject[element].dateEntry;
+  const description = entriesObject[element].description;
+   
+  
+    //creating HTML
+    const HTML = `
+    <div class="entry" id="entry-${element}">
+    <label for="title-entry-${element}"><strong>Title: </strong><p id="title-entry-${element}">${title}</p></label>
+    <br>
+    <label for="date-entry-${element}"><strong>Date: </strong><p id="date-entry-${element}">${date}</p></label>
+    <br>
+    <label for="description-entry-${element}"><strong>Description: </strong><p id="description-entry-${element}">${description}</p></label>
+    <br>
+    <button id="edit-button-${element}">Edit</button>
+    <button onclick="deleteEntry(${element})" id="delete-button-${element}">Delete</button>
+    </div>
+    `;
+  
+    entryContainerElement.insertAdjacentHTML("beforeend", HTML);
+  }
+
+if(localStorage.length){
+entriesObject = JSON.parse(localStorage.getItem('entryData'));
+entriesObject.forEach((element,index) => {
+  updateEntryContainerLocalStorage(index);
+});
+}
+
+//event listener
 
 //cancel functionality
 const cancel = () => {
@@ -69,3 +115,11 @@ const cancel = () => {
   entryContainerElement.classList.toggle("hidden");
   createEntryPageElement.innerHTML = "";
 };
+
+// delete button onclick function
+const deleteEntry = (counter)=>{
+const selectedEntry = document.getElementById(`entry-${counter}`);
+entriesObject.splice(`${counter}`,1);
+selectedEntry.remove();
+localStorage.setItem('entryData',JSON.stringify(entriesObject));
+}
